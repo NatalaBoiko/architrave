0857cea6a98f649b4764d4d14913dde8c2d28af5
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2023-07-12 01:23:00 +0300

    add 2 img and about/hero components

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,10 +1,45 @@
-import React from "react";
-import styles from "./About.module.scss";
+import Image from 'next/image';
+import React from 'react';
+import styles from './About.module.scss';
+import skyscraper from 'public/skyscraper.jpg';
 
 const About = () => {
   return (
-    <section>
-      <h2>About</h2>
+    <section className={styles.about}>
+      <h2 className={styles.aboutTitle}>Про нас</h2>
+      <div className={styles.aboutContent}>
+        <div className={styles.aboutList}>
+          <p className={styles.aboutText}>
+            Наша мета - перетворити ваші мрії у реальність шляхом створення
+            функціональних, естетичних та стильних будівель.
+          </p>
+          <p className={styles.aboutText}>
+            Маючи багаторічний досвід у галузі архітектури, ми здобули визнання
+            завдяки нашим успішним проектам.
+          </p>
+          <p className={styles.aboutText}>
+            Ми ретельно вивчаємо ваші потреби, враховуємо ваші бажання та
+            сприйняття краси, щоб створити проект, який відображатиме вашу
+            унікальну особистість та стиль.
+          </p>
+        </div>
+        <div className={styles.aboutImgContainer}>
+          <Image
+            src="/skyscraper.jpg"
+            alt="skyscraper"
+            fill={true}
+            className={styles.aboutImg}
+          />
+        </div>
+      </div>
+
+      <p className={styles.aboutText}>
+        Ми пропонуємо широкий спектр послуг, які охоплюють усі етапи
+        архітектурного процесу. Від початкового концептуального проектування до
+        завершення будівництва - ми забезпечуємо повний цикл послуг для
+        реалізації вашого проекту.
+      </p>
+      {/* </div> */}
     </section>
   );
 };
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,33 @@
-import React from "react";
-import styles from "./Hero.module.scss";
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import hero from 'public/heroBg.jpg';
+import styles from './Hero.module.scss';
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
-      <h1>Hero</h1>
+      <div className={styles.heroContainer}>
+        <Image
+          priority
+          src={hero}
+          alt="background hero"
+          className={styles.heroImg}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center bottom"
+        />
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>Творча майстерня АРХІТРАВ</h1>
+          <p className={styles.heroText}>
+            Creating comfort for <span className={styles.heroItem}>YOU</span>
+          </p>
+          <button type="button" className={styles.heroBtn}>
+            Залишити заявку
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
