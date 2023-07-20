9d4a7e015ba489951ae1a5bfe402a8ebcec45d1e
Nataliia <natalabojko@gmail.com>
2023-07-20 22:50:13 +0300

    slider start

--- src/sections/Gallery/Gallery.jsx
+++ src/sections/Gallery/Gallery.jsx
@@ -1,26 +1,12 @@
 import Image from "next/image";
 import React from "react";
 import styles from "./Gallery.module.scss";
+import { GalleryItem } from "./GalleryItem/GalleryItem";
 
 const Gallery = () => {
   return (
-    <section id="gallery">
-      <h2>Gallery</h2>
-      <Image
-        src="https://res.cloudinary.com/dtelxxva7/image/upload/v1689174904/architrave/73B7639F-729A-4E7A-A6A4-424228DD7501_fpkmwu.jpg"
-        alt="skyscraper"
-        width={300}
-        height={300}
-        className={styles.aboutImg}
-      />
-
-      <Image
-        src="https://res.cloudinary.com/dtelxxva7/image/upload/v1689174904/architrave/73B7639F-729A-4E7A-A6A4-424228DD7501_fpkmwu.jpg"
-        alt="skyscraper"
-        width={300}
-        height={300}
-        className={styles.aboutImg}
-      />
+    <section id="gallery" className={styles.gallery}>
+      <GalleryItem />
     </section>
   );
 };
