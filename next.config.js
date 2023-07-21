c1e9febec2e723a3be0df16117cd9767120995b6
Nataliia <natalabojko@gmail.com>
2023-07-21 14:15:21 +0300

    mobile header shadows

--- next.config.js
+++ next.config.js
@@ -5,26 +5,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: [{ loader: "@svgr/webpack" }],
-  //   });
-
-  //   return config;
-  // },
-
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     issuer: /\.[jt]sx?$/,
-  //     use: ["@svgr/webpack"],
-  //   });
-
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
