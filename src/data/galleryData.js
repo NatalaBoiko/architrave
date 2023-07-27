abf193168398ad281d934a8ad4447ed28e943a18
Nataliia <natalabojko@gmail.com>
2023-07-27 18:27:13 +0300

    add imgs

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -4,56 +4,53 @@ import { v4 } from "uuid";
 export const galleryData = [
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174914/architrave/55F1DCBB-974B-4F8E-BEAC-30DCBB1A5165_bbl7f3.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690470458/architrave/55F1DCBB-974B-4F8E-BEAC-30DCBB1A5165_bbl7f3_qkfdka.jpg",
   },
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174911/architrave/4A317CD4-487E-408F-93CC-4A5E40E97C56_sqtucm.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690470578/architrave/4A317CD4-487E-408F-93CC-4A5E40E97C56_sqtucm_e3mqnk.jpg",
   },
 
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174910/architrave/686AD554-4C37-48DA-8B5C-7AEBBFAC376B_cwkwc2.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690470721/architrave/686AD554-4C37-48DA-8B5C-7AEBBFAC376B_cwkwc2_hcgs6n.jpg",
   },
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174908/architrave/582335DA-D25B-43C5-A343-8DD9B5C95010_sbop0f.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690470782/architrave/582335DA-D25B-43C5-A343-8DD9B5C95010_sbop0f_kakjsi.jpg",
   },
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174907/architrave/3DD09E3F-48F9-4900-9AB6-935D312FCF63_onih3a.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471026/architrave/3DD09E3F-48F9-4900-9AB6-935D312FCF63_onih3a_mq6oje.jpg",
   },
-  // {
-  //   id: v4(),
-  //   url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174905/architrave/21562200-F4AD-40CC-BB36-7CEE44E12DDB_xbdbhj.jpg",
-  // },
+
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174904/architrave/73B7639F-729A-4E7A-A6A4-424228DD7501_fpkmwu.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471195/architrave/73B7639F-729A-4E7A-A6A4-424228DD7501_fpkmwu_vounzz.jpg",
   },
   {
     id: v4(),
-    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1689174902/architrave/9F5A164C-5D32-465A-B55A-8B078FD9185E_oxhfkl.jpg",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471095/architrave/9F5A164C-5D32-465A-B55A-8B078FD9185E_oxhfkl_w4mm3p.jpg",
   },
   {
     id: v4(),
-    url: "",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471303/architrave/BDDA3C77-A248-4490-9357-7EEADBBDCE8F_iy1iya_xtukye.jpg",
   },
   {
     id: v4(),
-    url: "",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471375/architrave/834F0D0D-F045-4381-BF82-BE6A2BEB5DC4_oqwgrn_wl0asp.jpg",
   },
   {
     id: v4(),
-    url: "",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471335/architrave/75244965-A21E-4552-BED4-FC6C19B5CBB0_n9fz6l_l8nwhx.jpg",
   },
   {
     id: v4(),
-    url: "",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471524/architrave/AC83BE16-18E7-4DA4-9095-FA96E9CB34E9_spq6iz_jxxuox.jpg",
   },
   {
     id: v4(),
-    url: "",
+    url: "https://res.cloudinary.com/dtelxxva7/image/upload/v1690471407/architrave/78F77A23-8186-469F-A57B-87E34CAAC67E_qxsd6f_klw8ip.jpg",
   },
   {
     id: v4(),
